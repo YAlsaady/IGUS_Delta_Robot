97a9d5b15882fc9740c946121ef8e7916df25ba8
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-09-05 04:03:15 +0200

    update

--- Modbus/igus_modbus.py
+++ Modbus/igus_modbus.py
@@ -10,6 +10,19 @@ Classes:
 Usage:
     To use this module, create an instance of the 'Robot' class with the IP address and the port of the Robot as a parameter.
 
+Example:
+    from igus_modbus import Robot
+    
+    # Create a Delta Robot instance with the IP address '192.168.1.11'
+    delta_robot = Robot('192.168.3.11')
+    
+    # Perform actions with the Delta Robot
+    delta_robot.enable()
+    delta_robot.reference()
+    delta_robot.set_position_endeffector(0, 0, 250)
+    delta_robot.set_velocity(120)
+    delta_robot.move_endeffector_absolute()
+
 """
 
 from pyModbusTCP.client import ModbusClient
