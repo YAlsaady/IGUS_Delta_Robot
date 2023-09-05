19705f4e024abf462f08f82a769ec4db4d36af88
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-09-05 02:31:41 +0200

    add a methode to set the target position of the endeffector

--- Modbus/igus_modbus.py
+++ Modbus/igus_modbus.py
@@ -66,3 +66,34 @@ class Robot:
         """
         self.client.write_single_coil(60, False)
         self.client.write_single_coil(60, True)
+
+    def set_position_endeffector(self, x_val, y_val, z_val):
+        """
+        Set the target position of the endeffector
+
+        This method sets the target position of the endeffector in millimeters.
+        It converts the input values to the appropriate format and writes them to registers 130-135.
+
+        The position can be absolute or relative to the the base or to itself.
+        To make the robot move, use the one of the folling methods
+        * move_endeffector_absolute()
+        * move_endeffector_relative_tool()
+        * move_endeffector_relative_base()
+
+        :param x_val: The target X position in millimeters.
+        :type x_val: float
+        :param y_val: The target Y position in millimeters.
+        :type y_val: float
+        :param z_val: The target Z position in millimeters.
+        :type z_val: float
+        :return: None
+        """
+        x_val *= 100
+        y_val *= 100
+        z_val *= 100
+        self.client.write_single_register(130, (x_val & 0x0000FFFF))
+        self.client.write_single_register(131, (x_val >> 16))
+        self.client.write_single_register(132, (y_val & 0x0000FFFF))
+        self.client.write_single_register(133, (y_val >> 16))
+        self.client.write_single_register(134, (z_val & 0x0000FFFF))
+        self.client.write_single_register(135, z_val >> 16)
