66e05022855723e68081fb194455e0a0b09a2196
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-09-06 01:15:29 +0200

    update

--- Modbus/example.py
+++ Modbus/example.py
@@ -1,11 +1,28 @@
 from igus_modbus import Robot
+import time
 
 # Create a Delta Robot instance with the IP address '192.168.1.11'
 delta_robot = Robot('192.168.3.11')
+dt = 0.5
 
 # Perform actions with the Delta Robot
+if not delta_robot.reference():
+    delta_robot.reference()
 delta_robot.enable()
-delta_robot.reference()
-delta_robot.set_position_endeffector(0, 0, 250)
-delta_robot.set_velocity(120)
+
+delta_robot.set_velocity(2000)
+delta_robot.set_position_endeffector(-200, -200, 150)
 delta_robot.move_endeffector_absolute()
+time.sleep(dt)
+
+delta_robot.set_position_endeffector(200, -200, 150)
+delta_robot.move_endeffector_absolute()
+time.sleep(dt)
+
+delta_robot.set_position_endeffector(200, 200, 150)
+delta_robot.move_endeffector_absolute()
+time.sleep(dt)
+
+delta_robot.set_position_endeffector(-200, 200, 150)
+delta_robot.move_endeffector_absolute()
+time.sleep(dt)
