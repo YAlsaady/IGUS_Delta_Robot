7c892235cf7d1137811d2ea6247e41cfb3b7d681
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-09-06 10:46:03 +0200

    Test with gripper

--- Modbus/example.py
+++ Modbus/example.py
@@ -1,27 +1,39 @@
 from igus_modbus import Robot
+from gripper import Gripper
 import time
 
 # Create a Delta Robot instance with the IP address '192.168.1.11'
 delta_robot = Robot("192.168.3.11")
-dt = 0.5
+gripper = Gripper()
 
 # Perform actions with the Delta Robot
-delta_robot.reference()
 delta_robot.enable()
+delta_robot.reference()
 
-delta_robot.set_velocity(2000)
-delta_robot.set_position_endeffector(-200, -200, 150)
-delta_robot.move_endeffector_absolute()
-time.sleep(dt)
+delta_robot.set_velocity(500)
+gripper.controll(50, 90)
 
-delta_robot.set_position_endeffector(200, -200, 150)
-delta_robot.move_endeffector_absolute()
-time.sleep(dt)
+delta_robot.set_position_endeffector(0, 0, 250)
+delta_robot.move_endeffector()
+gripper.open()
 
-delta_robot.set_position_endeffector(200, 200, 150)
-delta_robot.move_endeffector_absolute()
-time.sleep(dt)
+delta_robot.set_position_endeffector(0, 0, -120)
+delta_robot.move_endeffector(relative = "base")
+gripper.close()
+time.sleep(2)
 
-delta_robot.set_position_endeffector(-200, 200, 150)
-delta_robot.move_endeffector_absolute()
-time.sleep(dt)
+delta_robot.set_position_endeffector(0, 0, 120)
+delta_robot.move_endeffector(relative = "base")
+delta_robot.set_position_endeffector(200, 200, 0)
+delta_robot.move_endeffector(relative = "base")
+delta_robot.set_position_endeffector(0, 0, -130)
+delta_robot.move_endeffector(relative = "base")
+gripper.rotate(180)
+gripper.open()
+time.sleep(2)
+
+gripper.rotate(90)
+delta_robot.set_position_endeffector(0, 0, 100)
+delta_robot.move_endeffector(relative = "base")
+delta_robot.set_position_endeffector(0, 0, 250)
+delta_robot.move_endeffector()
