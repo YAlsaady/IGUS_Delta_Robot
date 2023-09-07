4706e256effbccca253d611de34e99f170d89acb
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-09-07 17:36:14 +0200

    Arduino(ESP32) program to controll the gripper

---
+++ Arduino/Gripper/Gripper.ino
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+#define SERVO 26
+#define SERVO_ROT 27
+#define CLOSE 10
+#define OPEN 170
+
+Servo gripper;
+Servo rotation;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(SERVO, OUTPUT);
+  pinMode(SERVO_ROT, OUTPUT);
+
+  gripper.attach(SERVO, 500, 2500);
+  rotation.attach(SERVO_ROT, 500, 2500);
+}
+
+void loop() {
+  static char reading[10];
+  static byte i = 0, val1 = CLOSE, val2 = 90;
+
+  while (Serial.available() == 0) {
+  }
+
+  while (Serial.peek() != ' ' && Serial.peek() != '\n') {
+    reading[i] = Serial.read();
+    i++;
+    delay(10);
+  }
+  reading[i] = '\0';
+
+  val1 = atoi(reading);
+
+  if (Serial.read() == ' ') {
+    i = 0;
+    while (Serial.peek() != ' ' && Serial.peek() != '\n') {
+      reading[i] = Serial.read();
+      i++;
+      delay(10);
+    }
+    reading[i] = '\0';
+    val2 = atoi(reading);
+  }
+
+  val1 = map(val1, 0, 100, OPEN, CLOSE);
+
+  gripper.write(val1);
+  rotation.write(val2);
+
+  i = 0;
+
+  while (Serial.available() != 0) {
+    Serial.read();
+  }
+}
